f3b4914b123f99e92d2c8d64911c71647b0aec50
SaketiAnnamnaidu16 <SaketiAnnamnaidu16@gmail.com>
2025-08-26 10:19:23 +0530

    AddedIngredientsInMealDetails

--- MealDetails/scrit.js
+++ MealDetails/scrit.js
@@ -17,10 +17,30 @@ async function fetchdata() {
 
   meals.forEach(item => {
     let data1 = `
-      <div class="details">
+    <div class="details">
+        <div class="image-details">
         <img src="${item.strMealThumb}" alt="image" class="detailImage">
-        <h1>${item.strMeal}</h1>
-
+      </div>
+      <div class="Content-Dettails">
+         <h1>${item.strMeal}</h1>
+         <span> </span>
+         <h3> Category : ${item.strCategory}</h3>
+         <h3> Source : <a href="https://www.youtube.com/watch?v=1IszT_guI08"> ${item.strYoutube}</a></h3>
+         <div class="ingredients">
+         <h3>Ingredients</h3>
+           <ol>
+            <li>${item.strIngredient1}</li>
+            <li>${item.strIngredient2}</li>
+            <li>${item.strIngredient3}</li>
+            <li>${item.strIngredient4}</li>
+            <li>${item.strIngredient5}</li>
+            <li>${item.strIngredient6}</li>
+            <li>${item.strIngredient7}</li>
+            <li>${item.strIngredient8}</li>
+          </ol>
+        </div>
+        
+      </div>
       </div>
     `;
 
